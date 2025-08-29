e7fac66cc893632baa75af5ee1c5abfbf990fb86
Aalwin <aalwin.mathew.thomas@gmail.com>
2025-08-29 20:48:36 +0530

    Made create_app() into __init__.py

--- app.py
+++ app.py
@@ -1,22 +1,6 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-      
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '490f5b3a23c26ce4d4457c046abc58d2'  
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///anecdotes.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+from app import create_app
 
-db=SQLAlchemy(app)
+app = create_app()
 
-from app.schema import db as schema_db, User, Story, Favorite, Progress
-
-with app.app_context():
-    db.create_all()
-
-      
-@app.route('/')
-def home():
-    return 'Welcome to Anecdotes!'
-      
 if __name__ == '__main__':
     app.run(debug=True)
--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,26 @@
-# with this file it says that this app folder is a package. i wasnt able to import  it at first into the app.py but after this file the python module recognises this folder as a python package hence the import error got resolved.
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    
+
+    app.config['SECRET_KEY'] = '490f5b3a23c26ce4d4457c046abc58d2'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///anecdotes.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+
+    from app.schema import User, Story, Favorite, Progress
+
+    with app.app_context():
+        db.create_all()
+
+    @app.route('/')
+    def home():
+        return 'Welcome to Anecdotes!'
+
+    return app
