887a919e034214fdaad85267563f81083367459d
Aalwin <aalwin.mathew.thomas@gmail.com>
2025-08-31 14:17:17 +0530

    Moved the database to the root directory

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,11 @@ import os
 login_manager = LoginManager()
 
 def create_app():
-    base_dir = os.path.abspath(os.path.dirname(__file__)) 
-    template_dir = os.path.join(base_dir, "..", "templates") 
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    template_dir = os.path.join(base_dir, "..", "templates")
+    instance_dir = os.path.abspath(os.path.join(base_dir, "..")) 
 
-    app = Flask(__name__,template_folder=template_dir)
+    app = Flask(__name__, template_folder=template_dir, instance_path=instance_dir)
 
     app.config['SECRET_KEY'] = 'df950b388400d919d9b673c5b42605bd923fd3e0703dae13d0dbbef06c2522e8'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///anecdotes.db'
